bd3a574e8a89e299cee6b3dadffffeb4e962b182
Shaine Gan <176516998+shainegan1407@users.noreply.github.com>
2026-01-19 17:37:59 +0800

    Added echo, changed exit

--- src/main/java/Cherry.java
+++ src/main/java/Cherry.java
@@ -1,17 +1,35 @@
+import java.util.Scanner;
+
 public class Cherry {
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void prompt() {
+        System.out.println("☆");
+        String command = scanner.nextLine();
+        if (command.equals("bye")) {
+            printMessage("See you next time, goodbye!");
+        } else {
+            printMessage(command);
+            prompt();
+        }
+    }
+
     public static void printMessage(String message) {
         String line = "____________________________________________________________\n";
         System.out.println(line);
         System.out.println(message);
         System.out.println(line);
     }
-    public static void main(String[] args) {
-        String logo = "\n          ___I_   \n"
-                + "         /\\-_--\\  \n"
-                + "        /  \\_-__\\ \n"
-                + " (•◡•)  |[]| [] | \n";
 
-        printMessage("Welcome to the task cafe!" + logo + "I'm cherry, how can I help you?");
-        printMessage("See you next time, goodbye!");
+    public static void main(String[] args) {
+        String logo = """
+                  ___I_
+                 /\\-_--\\ 
+                /  \\_-__\\
+         (•◡•)  |[]| [] |
+        """;
+
+        printMessage("Welcome to the task cafe!\n" + logo + "I'm cherry, how can I help you?");
+        prompt();
     }
 }
