b5edfceb68ff802ebb3023e33c4084020cb3c7de
Shaine Gan <176516998+shainegan1407@users.noreply.github.com>
2026-01-19 18:36:27 +0800

    Added tasks, display list

--- src/main/java/Cherry.java
+++ src/main/java/Cherry.java
@@ -2,14 +2,32 @@ import java.util.Scanner;
 
 public class Cherry {
     static Scanner scanner = new Scanner(System.in);
+    static String[] tasks = new String[100];
+    static int taskCount = 0;
+
+    public static String list() {
+        if (taskCount == 0) {
+            return "No tasks yet!";
+        }
+        StringBuilder list = new StringBuilder();
+        for (int i = 0; i < taskCount; i += 1) {
+            list.append((i + 1)).append(". ").append(tasks[i]).append("\n");
+        }
+        return list.toString();
+    }
 
     public static void prompt() {
         System.out.println("☆");
-        String command = scanner.nextLine();
-        if (command.equals("bye")) {
+        String input = scanner.nextLine();
+        if (input.equals("bye")) {
             printMessage("See you next time, goodbye!");
+        } else if (input.equals("list")) {
+            printMessage(list());
+            prompt();
         } else {
-            printMessage(command);
+            tasks[taskCount] = input;
+            taskCount += 1;
+            printMessage("New Task: " + input);
             prompt();
         }
     }
