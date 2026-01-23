32b949c9930f3104ba23c60623e36d657163ab45
Shaine Gan <176516998+shainegan1407@users.noreply.github.com>
2026-01-23 14:49:04 +0800

    Changed array to ArrayList, Added delete

--- src/main/java/Cherry.java
+++ src/main/java/Cherry.java
@@ -1,8 +1,9 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Cherry {
     static Scanner scanner = new Scanner(System.in);
-    static Task[] tasks = new Task[100];
+    static ArrayList<Task> tasks = new ArrayList<>();
     static int taskCount = 0;
 
     public static String list() {
@@ -11,7 +12,7 @@ public class Cherry {
         }
         StringBuilder list = new StringBuilder();
         for (int i = 0; i < taskCount; i += 1) {
-            list.append((i + 1)).append(". ").append(tasks[i].toString()).append('\n');
+            list.append((i + 1)).append(". ").append(tasks.get(i).toString()).append('\n');
         }
         return list.toString();
     }
@@ -25,35 +26,30 @@ public class Cherry {
         try {
             switch (command) {
                 case "todo" -> {
-                    tasks[taskCount] = new Task(tokens);
+                    tasks.add(new Task(tokens));
                     taskCount += 1;
-                    printMessage("New Task: " + tasks[taskCount - 1].toString());
+                    printMessage("New Task: " + tasks.get(taskCount - 1).toString());
                     prompt();
                 }
                 case "event" -> {
-                    tasks[taskCount] = new Event(tokens);
+                    tasks.add(new Event(tokens));
                     taskCount += 1;
-                    printMessage("New Task: " + tasks[taskCount - 1].toString());
+                    printMessage("New Task: " + tasks.get(taskCount - 1).toString());
                     prompt();
                 }
                 case "deadline" -> {
-                    tasks[taskCount] = new Deadline(tokens);
+                    tasks.add(new Deadline(tokens));
                     taskCount += 1;
-                    printMessage("New Task: " + tasks[taskCount - 1].toString());
-                    prompt();
-                }
-                case "bye" -> printMessage("See you next time, goodbye!");
-                case "list" -> {
-                    printMessage(list());
+                    printMessage("New Task: " + tasks.get(taskCount - 1).toString());
                     prompt();
                 }
                 case "mark" -> {
                     if (tokens.length > 1) {
                         int taskNumber = Integer.parseInt(tokens[1]);
                         if (0 < taskNumber && taskNumber < 101) {
-                            tasks[taskNumber - 1].markTask();
+                            tasks.get(taskNumber - 1).markTask();
                             printMessage("Good job! I've marked this task as done:\n"
-                                    + tasks[taskNumber - 1].toString());
+                                    + tasks.get(taskNumber - 1).toString());
                         } else {
                             throw new CherryException("This task does not exist, did you mean something else?");
                         }
@@ -66,8 +62,9 @@ public class Cherry {
                     if (tokens.length > 1) {
                         int taskNumber = Integer.parseInt(tokens[1]);
                         if (0 < taskNumber && taskNumber < 101) {
-                            tasks[taskNumber - 1].unmarkTask();
-                            printMessage("Alright, I've unmarked this task:\n" + tasks[taskNumber - 1].toString());
+                            tasks.get(taskNumber - 1).unmarkTask();
+                            printMessage("Alright, I've unmarked this task:\n"
+                                    + tasks.get(taskNumber - 1).toString());
                         } else {
                             throw new CherryException("This task does not exist, did you mean something else?");
                         }
@@ -76,6 +73,28 @@ public class Cherry {
                     }
                     prompt();
                 }
+                case "delete" -> {
+                    if (tokens.length > 1) {
+                        int taskNumber = Integer.parseInt(tokens[1]);
+                        if (0 < taskNumber && taskNumber < 101) {
+                            taskCount -= 1;
+                            printMessage("Alright, I've deleted this task:\n"
+                                    + tasks.get(taskNumber - 1).toString()
+                                    + "Now you have " + taskCount + " tasks in the list\n");
+                            tasks.remove(taskNumber - 1);
+                        } else {
+                            throw new CherryException("This task does not exist, did you mean something else?");
+                        }
+                    } else {
+                        throw new CherryException("You will need to specify the task number");
+                    }
+                    prompt();
+                }
+                case "list" -> {
+                    printMessage(list());
+                    prompt();
+                }
+                case "bye" -> printMessage("See you next time, goodbye!");
                 default -> throw new CherryException("I'm sorry, the task cafe can't help you with this yet");
 
             }
