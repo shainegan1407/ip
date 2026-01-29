ef91efefde290df671070fc2b0aae5dabf820910
Shaine Gan <176516998+shainegan1407@users.noreply.github.com>
2026-01-29 21:22:13 +0800

    WIP: local changes to DeadlineTest

---
+++ src/test/java/DeadlineTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class DeadlineTest {
+    @Test
+    public void deadlineInitialisation_localDate_success() throws Exception {
+        assertEquals("(D) | [ ] | return book
+                "                + this.description\n" +
+                "                + \" | by:\" + this.deadline")
+    }
+}
